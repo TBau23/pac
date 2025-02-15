1e685b20d940bb07013857493c5f7be952d6ffd4
TBau23 <tom.a.bauer@protonmail.com>
2025-02-14 19:28:37 -0600

    move dat boi

--- pacman.py
+++ pacman.py
@@ -1,12 +1,45 @@
+import pygame
+
 class Pacman:
     def __init__(self, start_x, start_y, level):
         self.x = start_x
         self.y = start_y
         self.level = level
-        self.direction = None
+        self.direction = 'right'
         self.score = 0
         self.lives = 3
         self.dead = False
     
-    def move(self, direction):
+    def handle_keypress(self, event):
+        if event.key == pygame.K_RIGHT:
+            if self.is_valid_move(self.x + 1, self.y):
+                self.x += 1
+                self.direction = 'right'
+            else:
+                self.direction = None
+        elif event.key == pygame.K_LEFT:
+            if self.is_valid_move(self.x - 1, self.y):
+                self.x -= 1
+                self.direction = 'left'
+            else:
+                self.direction = None
+        elif event.key == pygame.K_UP:
+            if self.is_valid_move(self.x, self.y - 1):
+                self.y -= 1
+                self.direction = 'up'
+            else:
+                self.direction = None
+        elif event.key == pygame.K_DOWN:
+            if self.is_valid_move(self.x, self.y + 1):
+                self.y += 1
+                self.direction = 'down'
+            else:
+                self.direction = None
+    
+    def is_valid_move(self, x, y):
         pass
+    
+
+
+
+    
